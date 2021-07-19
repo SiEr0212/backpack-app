3b2da1945656c45409c15a8cd3fb682c852bd425
SiEr0212 <simonertel@gmx.de>
2021-07-19 23:36:40 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Backpack App</h1>
         <h2>Organize your Backpack for your next Hike</h2>
+        
       </header>
     </div>
   );
