ee369e3b3ef2e00bad9e0fb849d1f20f3b6ff81a
SiEr0212 <simonertel@gmx.de>
2021-07-20 23:58:56 +0200

    styling app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Backpack App</h1>
         <h2>Organize your Backpack for your next Hike</h2>
-        <button>add item to backpack</button>
+        <button>add an item to backpack</button>
         <br />
         <button>remove item from backpack</button>
       </header>
