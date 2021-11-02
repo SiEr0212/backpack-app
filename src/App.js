8186c16540558132dcfb5cda9fc1fd91c60e7746
SiEr0212 <simonertel@gmx.de>
2021-11-02 21:03:12 +0100

    adding addition functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import "./App.css";
 import React, { useState } from 'react';
 
 function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [total, setTotal] = useState(number1 + number2);
+
   return (
     <div className="App">
       <header className="App-header">
