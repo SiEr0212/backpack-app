4ef4205432942993eb4ecfe588488d0a78c75eef
SiEr0212 <simonertel@gmx.de>
2021-11-02 21:26:33 +0100

    setting up an editible list

--- src/App.js
+++ src/App.js
@@ -2,7 +2,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { useState } from 'react';
 
+
+const initialList = [
+  {
+    id: 'a',
+    name: 'Sleepingbag',
+  },
+  {
+    id: 'b',
+    name: 'Tent',
+  },
+];
+
 function App() {
+  const [list, setList] = useState(initialList);
+
+  function handleChange() {
+    // track input field's state
+  }
+ 
+  function handleAdd() {
+    // add item
+  }
 
   return (
     <div className="App">
@@ -12,13 +33,25 @@ function App() {
         <h1>Backpack App</h1>
         <br />
         <br />
-       
         <h2>Organize your Backpack for your next Hike</h2>
         <br />
         <br />
-        <br />
-        <br />
+        <div>
+          <input type="text" onChange={handleChange} />
+          <button type="button" onClick={handleAdd}>
+            Add
+          </button>
+        </div>
+        <ul>
+      {list.map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
        
+
+
+
+
       <button>Add to the Backpack!</button>
 
       <h2>Total weight: 0kg</h2>
