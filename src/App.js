c71d35952c3180e085a3d3165ea8648405816205
SiEr0212 <simonertel@gmx.de>
2021-11-02 21:05:38 +0100

    adding addition functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
   const [number2, setNumber2] = useState(0);
   const [total, setTotal] = useState(number1 + number2);
 
+  function calculateTotal() {
+    setTotal(number1 + number2);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +21,24 @@ function App() {
         <br />
         <button>remove item from backpack</button>
         <br />
+        <div className="number-inputs">
+        <input
+          type="number"
+          value={number1}
+          onChange={e => setNumber1(+e.target.value)}
+          placeholder="0"
+        />
+        <input
+          type="number"
+          value={number2}
+          onChange={e => setNumber2(+e.target.value)}
+          placeholder="0"
+        />
+      </div>
+
+      <button onClick={calculateTotal}>Add Them!</button>
+
+      <h2>{total}</h2>
       </header>
     </div>
   );
