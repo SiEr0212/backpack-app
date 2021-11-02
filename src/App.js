43a87a9424871df7066705525ad64848e229b720
SiEr0212 <simonertel@gmx.de>
2021-11-02 21:20:51 +0100

    changing structutre to editible list

--- src/App.js
+++ src/App.js
@@ -3,33 +3,25 @@ import "./App.css";
 import React, { useState } from 'react';
 
 function App() {
-  const [number1, setNumber1] = useState(0);
-  const [total, setTotal] = useState(0 + number1);
-
-  function calculateTotal() {
-    setTotal( 0 + number1);
-  }
 
   return (
     <div className="App">
+
+  
       <header className="App-header">
         <h1>Backpack App</h1>
-        <h2>Organize your Backpack for your next Hike</h2>
-        
-        <div className="number-inputs">
-        <input
-          type="number"
-          value={number1}
-          onChange={e => setNumber1(+e.target.value)}
-          placeholder="0"
-        />
+        <br />
+        <br />
        
-        
-      </div>
+        <h2>Organize your Backpack for your next Hike</h2>
+        <br />
+        <br />
+        <br />
+        <br />
+       
+      <button>Add to the Backpack!</button>
 
-      <button onClick={calculateTotal}>Add to the Backpack!</button>
-
-      <h2>Total weight: {total}kg</h2>
+      <h2>Total weight: 0kg</h2>
       </header>
     </div>
   );
