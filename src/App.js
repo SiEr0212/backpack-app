9652e71ea6f9144f8eb4aac1f16fe84a67be052a
SiEr0212 <simonertel@gmx.de>
2021-11-02 21:32:57 +0100

    setting up an editible list

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
+mport { v4 as uuidv4 } from 'uuid';
 
 
 const initialList = [
   {
-    id: 'a',
-    name: 'Sleepingbag',
+    id: "a",
+    name: "Sleepingbag",
   },
   {
-    id: 'b',
-    name: 'Tent',
+    id: "b",
+    name: "Tent",
   },
 ];
 
 function App() {
   const [list, setList] = useState(initialList);
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
 
   function handleChange(e) {
     setName(e.target.value);
   }
- 
+
   function handleAdd() {
     const newList = list.concat({ name });
- 
+
     setList(newList);
   }
 
   return (
     <div className="App">
-
-  
       <header className="App-header">
         <h1>Backpack App</h1>
         <br />
@@ -40,24 +39,18 @@ function App() {
         <br />
         <br />
         <div>
-        <input type="text" value={name} onChange={handleChange} />
+          <input type="text" value={name} onChange={handleChange} />
           <button type="button" onClick={handleAdd}>
-          Add to the Backpack!
+            Add to the Backpack!
           </button>
         </div>
         <ul>
-      {list.map((item) => (
-        <li key={item.id}>{item.name}</li>
-      ))}
-    </ul>
-       
+          {list.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
 
-
-
-
-     
-
-      <h2>Total weight: 0kg</h2>
+        <h2>Total weight: 0kg</h2>
       </header>
     </div>
   );
