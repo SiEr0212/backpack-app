7284db3610baa3bce4a9ee81e1e19bdff4fef1cb
SiEr0212 <simonertel@gmx.de>
2021-11-04 23:59:30 +0100

    adding styling

--- src/App.js
+++ src/App.js
@@ -117,19 +117,19 @@ function App() {
 
         <h2>Total weight: {finalWeightTotal / 1000} kg</h2>
         <Typography component="legend">Controlled</Typography>
-<Rating
-  name="simple-controlled"
-  value={value}
-  onChange={(event, newValue) => {
-    setValue(newValue);
-  }}
-/>
-<Typography component="legend">Read only</Typography>
-<Rating name="read-only" value={value} readOnly />
-<Typography component="legend">Disabled</Typography>
-<Rating name="disabled" value={value} disabled />
-<Typography component="legend">No rating given</Typography>
-<Rating name="no-value" value={null} />
+        <Rating
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        />
+        <Typography component="legend">Read only</Typography>
+        <Rating name="read-only" value={value} readOnly />
+        <Typography component="legend">Disabled</Typography>
+        <Rating name="disabled" value={value} disabled />
+        <Typography component="legend">No rating given</Typography>
+        <Rating name="no-value" value={null} />
       </header>
     </div>
   );
