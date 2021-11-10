283d452941d44790b0bbf4061b5293663bb2caf6
SiEr0212 <simonertel@gmx.de>
2021-11-10 23:35:51 +0100

    repairing connection to database

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const cors = require("cors");
+
+const users = require("./routes/api/users");
+
 const app = express();
 
 // Bodyparser middleware
@@ -13,8 +16,22 @@ const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log("MongoDB successfully connected"))
-  .catch((err) => console.log(err));
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
