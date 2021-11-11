b9faf9ea2dab1c85d326ae285c8447bb4ad841ee
SiEr0212 <simonertel@gmx.de>
2021-11-11 02:32:24 +0100

    qra

--- server/server.js
+++ server/server.js
@@ -6,9 +6,22 @@ const users = require("./routes/api/users");
 
 const app = express();
 
-// Bodyparser middleware
-app.use(express.urlencoded({ extended: true }));
+/* app.use(Cors()); */
+app.use(
+  cors({
+    origin: (origin, callback) => callback(null, true),
+    credentials: true
+  })
+);
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -29,6 +42,7 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 
 
-const port = process.env.PORT || 5000;
+const port =  5000; //process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
