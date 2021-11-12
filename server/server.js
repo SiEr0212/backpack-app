e40b6c09b2ed3406aea3bcc7dd59910e0b23909e
SiEr0212 <simonertel@gmx.de>
2021-11-12 23:35:07 +0100

    cleaning up server.js

--- server/server.js
+++ server/server.js
@@ -26,14 +26,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 //from dev
 app.get("/home", (req, res) => {
   res.json({
