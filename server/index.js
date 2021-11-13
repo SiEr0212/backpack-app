39446c1f55a6fb9b44abd08666405571d3319826
SiEr0212 <simonertel@gmx.de>
2021-11-13 17:59:38 +0100

    remaking backend connection

--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 5000; 
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`) )
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
