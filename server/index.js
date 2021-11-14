7f8ff3a8534b674a35f268a22a6c5fda3c939ade
SiEr0212 <simonertel@gmx.de>
2021-11-14 21:18:19 +0100

    reconnectingBackend

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-dotenv.config()//reads the dotenv file
+dotenv.config(); //reads the dotenv file
 
 //set up server
 
@@ -13,6 +13,6 @@ app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
 //connect to mongoDB
 //installesd "npm i dotenv" to acces env
 mongoose.connect(process.env.MDB_CONNECT, (err) => {
-    if(err) console.error(err);
-    console.log("Connected to MongoDB!)
-} );
+  if (err) console.error(err);
+  console.log("Connected to MongoDB");
+});
