2c13fdd1b158806bc149945abc006535df167c02
SiEr0212 <simonertel@gmx.de>
2021-11-14 21:06:21 +0100

    reconnectingBackend

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+//set up server
+
 const app = express();
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
 
-app.get("/test", (req, res) => {
-  res.send("It works");
-});
+
