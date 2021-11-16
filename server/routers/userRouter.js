41ff31865c11d9795c9feb97af6c6d7cec677ad9
SiEr0212 <simonertel@gmx.de>
2021-11-16 23:50:07 +0100

    setting up authentification

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 router.post("/", (req, res) => {
   try {
     const { email, password, passwordVerify } = req.body;
+
+    //validation:
   } catch (err) {
     console.error(err);
-    res.status(500).send()
+    res.status(500).send();
   }
 });
 
