9ce0f14e687d2d903a0ed0a55a58dee83fadbf4d
SiEr0212 <simonertel@gmx.de>
2021-11-16 23:54:34 +0100

    setting up authentification

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -5,6 +5,10 @@ router.post("/", (req, res) => {
     const { email, password, passwordVerify } = req.body;
 
     //validation:
+    if(!email || !password || !passwordVerify) 
+    return res.status(400).json({errorMessage: "Please enter all required fields"})
+
+    
   } catch (err) {
     console.error(err);
     res.status(500).send();
