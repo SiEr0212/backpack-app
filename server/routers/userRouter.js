770754ec968726a31c259e4ff97b6a4a2564e039
SiEr0212 <simonertel@gmx.de>
2021-11-16 23:58:58 +0100

    setting up authentification

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -11,11 +11,9 @@ router.post("/", (req, res) => {
         .json({ errorMessage: "Please enter all required fields." });
 
     if (password.length < 6)
-      return res
-        .status(400)
-        .json({
-          errorMessage: "Please enter a password of at least 6 characters.",
-        });
+      return res.status(400).json({
+        errorMessage: "Please enter a password of at least 6 characters.",
+      });
   } catch (err) {
     console.error(err);
     res.status(500).send();
