672eb4a221ca2a7778ceeff277f33b0d17cca5f3
SiEr0212 <simonertel@gmx.de>
2021-11-17 23:42:33 +0100

    implementing validation

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -21,10 +21,13 @@ router.post("/", (req, res) => {
         errorMessage: "Please enter the same password twice.",
       });
 
-    // checks if the email we just entered matches the email of an existing user
+   /*  checks if the email we just entered matches the email of an existing user
+     and if thats the case it is saved in the variabel existingUser.
+     so if its null no preexisting user */
+
     const existingUser = User.findOne({ email });
 
-    
+
   } catch (err) {
     console.error(err);
     res.status(500).send();
