89d42d1a4541fb4d3cc3af3c12f84145a3ba05a9
SiEr0212 <simonertel@gmx.de>
2021-11-20 23:35:42 +0100

    implementing login

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.post("/", async (req, res) => {
   try {
