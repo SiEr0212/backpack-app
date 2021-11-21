2d9d6a83dd8a8e1d73c41f78ebbeb3c234fa0968
SiEr0212 <simonertel@gmx.de>
2021-11-21 18:58:48 +0100

    adding login route

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -117,7 +117,8 @@ router.post("/login", async (req, res) => {
       res.cookie("token", "", {
         httpOnly: true,
         expires: new Date(0), //places it in the past so it expires directly => browser will completly remove the cookie
-      });
+      })
+      .send();
     });
   } catch (err) {
     console.error(err);
