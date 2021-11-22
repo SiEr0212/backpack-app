d4de9a89036a59123389077d48aca9a8989f1cd1
SiEr0212 <simonertel@gmx.de>
2021-11-22 21:31:50 +0100

    adding new model

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,6 @@
 function auth(req, res, next) {
   try {
-    console.log(req.cookies); //installed library with npm i cookie-parser to store the cookie in an json object
+   const token = req.cookies.token; //installed library with npm i cookie-parser to store the cookie in an json object
   } catch (err) {
     console.log(err);
     res.status(401).json({
