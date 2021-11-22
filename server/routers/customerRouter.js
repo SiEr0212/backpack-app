b757c8770ce6b9e8b001f85b6e30f1cd3c671c4c
SiEr0212 <simonertel@gmx.de>
2021-11-22 20:18:33 +0100

    adding new model

--- server/routers/customerRouter.js
+++ server/routers/customerRouter.js
@@ -1,4 +1,12 @@
 const router = require("express").Router();
+const Customer = require("../models/customerModel");
 
-
+router.post("/", async (req, res) => {
+  try {
+    const { name } = req.body;
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
 module.exports = router;
