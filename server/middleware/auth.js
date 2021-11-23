29f69c131c38de8778bd522bc59015af3e0ae917
SiEr0212 <simonertel@gmx.de>
2021-11-23 16:00:29 +0100

    implementing protected routes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,9 +1,13 @@
+const jwt = require("jsonwebtoken");
+
 function auth(req, res, next) {
   try {
    const token = req.cookies.token; //installed library with npm i cookie-parser to store the cookie in an json object
 
    if (!token) return res.status(401).json({errorMesage: "Unauthorized"});
+
    
+
   } catch (err) {
     console.log(err);
     res.status(401).json({
