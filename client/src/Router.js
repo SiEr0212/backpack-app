6a4bbdb2ecd0c6655aad933c9c87acaca5666d7b
SiEr0212 <simonertel@gmx.de>
2021-11-24 16:24:18 +0100

    adding client routing

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+
 
 export default function Router() {
-    return 
-        <>
-            
-        </>
-    
+  return
+  <></>;
 }
