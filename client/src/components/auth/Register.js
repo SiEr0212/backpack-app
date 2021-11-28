26fe12378d51a905408c548c8b2971be7cd95f1c
SiEr0212 <simonertel@gmx.de>
2021-11-28 23:49:49 +0100

    adding register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 export default function Register() {
-    return (
-        <div>
-          <h1>Register a new account</h1>  
-          <form >
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Verify your password" />
-          <button type="submit">Register</button>
-          </form>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Register a new account</h1>
+      <form>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Verify your password" />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
