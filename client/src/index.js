2dd6ce8bae6e7ad001f6989f8b0b8ba20ded92d9
SiEr0212 <simonertel@gmx.de>
2021-12-04 15:50:39 +0100

    finishing registration

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
+
 import App from "./App";
 
 ReactDOM.render(
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
-
+const cors = require("cors");
 dotenv.config(); //reads the dotenv file
 
 //set up server
