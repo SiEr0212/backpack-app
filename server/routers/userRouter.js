f17c062c92f67ed7decb39317fc4676c6718bcea
SiEr0212 <simonertel@gmx.de>
2021-12-06 00:18:45 +0100

    creating protected routes

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -128,6 +128,27 @@ router.post("/login", async (req, res) => {
         })
         .send();
     });
+
+router.get("/loggedIn", (req,res) => {
+  try {
+    const token = req.cookies.token; //installed library with npm i cookie-parser to store the cookie in an json object
+
+    if (!token) return res.status(401).json({ errorMesage: "Unauthorized" });
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = verified.user;
+
+    next(); //exits the auth middleware and lets the rest execute 
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({
+      errorMesage: "Unauthorized",
+    });
+  }
+
+})
+    
   
 
 module.exports = router;
