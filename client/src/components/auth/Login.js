7835dde4bc35afa5f58b9dd59679ee36593828bc
SiEr0212 <simonertel@gmx.de>
2021-12-05 23:49:08 +0100

    creating frontend login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 
-export default function Register() {
+export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordVerify, setPasswordVerify] = useState("");
