2a9e461f5d26eb8ef666b85721769963b4804ae4
SiEr0212 <simonertel@gmx.de>
2021-12-05 23:51:54 +0100

    creating frontend login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -25,7 +25,7 @@ export default function Login() {
   return (
     <div>
       <h1>Login to your account</h1>
-      <form onSubmit={register}>
+      <form onSubmit={login}>
         <input
           type="email"
           placeholder="Email"
