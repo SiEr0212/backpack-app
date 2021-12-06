f7199804339cf55f4c1c0244f17462e3d30b688c
SiEr0212 <simonertel@gmx.de>
2021-12-06 23:27:58 +0100

    implementing authcontext

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,11 +1,13 @@
 import React from "react";
+import axios from "axios";
 
 export default function AuthContextProvider() {
 
   const [loggedIn, setLoggedIn] = useState(undefined);
   
   async function getLoggedIn() {
-      
+
+    const loggedInRes = await axios.get("http://localhost:5000/auth/")
   }
   return <>
   
