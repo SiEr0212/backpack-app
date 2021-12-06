12143858fe750c6cebadec368be39c2c847a5de8
SiEr0212 <simonertel@gmx.de>
2021-12-06 23:42:57 +0100

    implementing authcontext

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Router from "./Router";
 import axios from "axios";
+import {  AuthContextProvider } from "./context/AuthContext";
 
 axios.defaults.withCredentials = true;
 
 function App() {
   return(
-    <>  
+    <AuthContextProvider>  
     
     <Router />
     
-    </>
+    </AuthContextProvider>
    
     
     
