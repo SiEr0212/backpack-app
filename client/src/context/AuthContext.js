f4d27b5b395e98531f179d87c0c80dabbc828ffa
SiEr0212 <simonertel@gmx.de>
2021-12-06 23:28:58 +0100

    implementing authcontext

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -6,6 +6,7 @@ export default function AuthContextProvider() {
 
   async function getLoggedIn() {
     const loggedInRes = await axios.get("http://localhost:5000/auth/");
+    setLoggedIn(loggedInRes.data)//set it to the boolean true or false
   }
   return <></>;
 }
