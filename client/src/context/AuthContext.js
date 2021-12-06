8df3898a4f1b637cec271b79190fed0042062fc0
SiEr0212 <simonertel@gmx.de>
2021-12-06 23:25:15 +0100

    implementing authcontext

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function AuthContextProvider() {
 
   const [loggedIn, setLoggedIn] = useState(undefined);
-
+  
+  async function getLoggedIn() {
+      
+  }
   return <>
   
   </>;
