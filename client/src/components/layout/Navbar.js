a91295ff5a129f6c0731be344c1f64a14b22f65e
SiEr0212 <simonertel@gmx.de>
2021-12-07 23:59:55 +0100

    implementing authcontext

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 
 export default function Navbar() {
+    
   const loggedIn = useContext(AuthContext);
 
   return (
