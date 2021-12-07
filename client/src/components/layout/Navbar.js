6e5b5cf988c4d7a96195bf1b4da3724c3a05db57
SiEr0212 <simonertel@gmx.de>
2021-12-07 23:59:28 +0100

    implementing authcontext

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,18 +1,16 @@
-import React, { useContext } from 'react';
-import {Link} from "react-router-dom";
-
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const loggedIn = useContext();
 
-    const loggedIn = useContext();
-    
-    return (
-        <div>
-           <Link to="/">Home</Link>
-           <Link to="/register">Register</Link> 
-           <Link to="/login">Login</Link> 
-           <Link to="/customer">Customer</Link>
-           <Link to="/backpack">Backpack</Link>  
-        </div>
-    )
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/customer">Customer</Link>
+      <Link to="/backpack">Backpack</Link>
+    </div>
+  );
 }
