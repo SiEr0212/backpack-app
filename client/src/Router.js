70218e15bb75cf6bb86b3737bd70275fa9dd474b
SiEr0212 <simonertel@gmx.de>
2021-12-09 23:24:04 +0100

    implementing protected navbar

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {useContext} from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Backpack from "./components/Backpack";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import AuthContext from "./context/AuthContext";
 
 function Router() {
+
+const {loggedIn} = useContext(AuthContext)
+
   return (
     <>
       <Navbar />
