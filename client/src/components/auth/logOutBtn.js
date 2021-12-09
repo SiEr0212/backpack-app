74ba635ad1e2552a8c4d5f935d1da0b1ca029dcf
SiEr0212 <simonertel@gmx.de>
2021-12-09 23:34:35 +0100

    implementing protected navbar

--- client/src/components/auth/logOutBtn.js
+++ client/src/components/auth/logOutBtn.js
@@ -1,9 +1,6 @@
-import React from 'react';
+import React from "react";
 
 export default function logOutBtn() {
-    return (
-        <button>
-          Logout  
-        </button>
-    )
+  function logout() {}
+  return <button>Logout</button>;
 }
