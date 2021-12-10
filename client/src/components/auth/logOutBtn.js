4d0a2fed2a995ac1ed25cda474ffef4c40482cd0
SiEr0212 <simonertel@gmx.de>
2021-12-10 23:01:50 +0100

    adding logout button

--- client/src/components/auth/logOutBtn.js
+++ client/src/components/auth/logOutBtn.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function logOutBtn() {
   async function logout() {
-
+await axios.get("http://localhost:5000/auth/logout");//returns an empty cookie
 
   }
 
