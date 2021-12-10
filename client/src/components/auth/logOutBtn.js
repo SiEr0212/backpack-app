6fa9a20c2d9ab9845d92f05f374de09ba1811c62
SiEr0212 <simonertel@gmx.de>
2021-12-10 23:07:08 +0100

    adding logout button

--- client/src/components/auth/logOutBtn.js
+++ client/src/components/auth/logOutBtn.js
@@ -3,11 +3,12 @@ import React, { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
 
 export default function logOutBtn() {
-  
+
   const { getLoggedIn } = useContext(AuthContext);
 
   async function logout() {
     await axios.get("http://localhost:5000/auth/logout"); //returns an empty cookie
+    getLoggedIn();
   }
 
   return <button onClick={logout}>Logout</button>;
