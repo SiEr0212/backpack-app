b007eaaa34cbdc6663d307431555b7b431ab2bde
SiEr0212 <simonertel@gmx.de>
2021-12-11 23:46:49 +0100

    adding customer component

--- client/src/components/customers/Customers.js
+++ client/src/components/customers/Customers.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import CustomerForm from "./CustomerForm";
 
 export default function Customers() {
     return (
         <div>
-            
+           <CustomerForm />
         </div>
     )
 }
