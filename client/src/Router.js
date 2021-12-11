60f7751b313cac72d0243fa60a1002baee30b1f6
SiEr0212 <simonertel@gmx.de>
2021-12-11 23:48:43 +0100

    adding customer component

--- client/src/Router.js
+++ client/src/Router.js
@@ -5,6 +5,7 @@ import Backpack from "./components/Backpack";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import AuthContext from "./context/AuthContext";
+import Customers from "./components/customers/Customers";
 
 function Router() {
   const { loggedIn } = useContext(AuthContext);
@@ -24,7 +25,7 @@ function Router() {
         )}
         {loggedIn === true && (
           <>
-            <Route path="/customer" />
+            <Route path="/customer" element={<Customers />} />
 
             <Route path="/backpack" element={<Backpack />} />
           </>
