31adad3e4d81b8a5d4e00f045d5abef0005583ca
SiEr0212 <simonertel@gmx.de>
2021-12-11 23:58:47 +0100

    adding customer component

--- client/src/components/customers/CustomerForm.js
+++ client/src/components/customers/CustomerForm.js
@@ -1,18 +1,19 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 export default function CustomerForm() {
+  const [customerName, setCustomerName] = useState("");
 
-const [customerName, setCustomerName] = useState("");
-
-
-    return (
-        <div>
-           <form>
-           <input type="text" placeholder='username' onChange={(e)=> setCustomerName(e.value.target)}/>
-           <button type='submit'>Save new user</button>
-           </form>
-        </div>
-        
-    )
-    console.log(customerName)
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="username"
+          onChange={(e) => setCustomerName(e.value.target)}
+        />
+        <button type="submit">Save new user</button>
+      </form>
+    </div>
+  );
+  console.log(customerName);
 }
