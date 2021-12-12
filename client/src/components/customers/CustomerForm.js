5897443772d36f78f6ffd8dabf285e8cf448f809
SiEr0212 <simonertel@gmx.de>
2021-12-12 23:38:08 +0100

    adding customer forms

--- client/src/components/customers/CustomerForm.js
+++ client/src/components/customers/CustomerForm.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function CustomerForm() {
   const [customerName, setCustomerName] = useState("");
 
   async function saveCustomer(e) {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      
-    } catch (err) {
-      console.error(err)
-      
-    }
+      await axios.post(
 
-  };
+      )
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   return (
     <div>
@@ -20,12 +21,13 @@ export default function CustomerForm() {
         <input
           type="text"
           placeholder="username"
-          onChange={(e) => {setCustomerName(e.value.target)}}
-          value = {customerName}
+          onChange={(e) => {
+            setCustomerName(e.value.target);
+          }}
+          value={customerName}
         />
         <button type="submit">Save new user</button>
       </form>
     </div>
   );
-  
 }
