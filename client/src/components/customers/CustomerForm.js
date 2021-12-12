12a33de377b3f7e97134c170c1907682d4b124ef
SiEr0212 <simonertel@gmx.de>
2021-12-12 23:40:43 +0100

    adding customer forms

--- client/src/components/customers/CustomerForm.js
+++ client/src/components/customers/CustomerForm.js
@@ -7,7 +7,7 @@ export default function CustomerForm() {
   async function saveCustomer(e) {
     e.preventDefault();
     try {
-      const customerData = {}
+      const customerData = {};
       await axios.post("http://localhost:5000/customer/");
     } catch (err) {
       console.error(err);
