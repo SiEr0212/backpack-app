3912df524f14e4805c2b61f662529e87adb897f2
SiEr0212 <simonertel@gmx.de>
2021-12-14 00:00:43 +0100

    listing the customers

--- client/src/components/customers/Customers.js
+++ client/src/components/customers/Customers.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CustomerForm from "./CustomerForm";
 import CustomerList from "./CustomerList";
 
@@ -14,7 +14,7 @@ export default function Customers() {
   useEffect(() =>{
       getCustomers()
   }, [])
-  
+
   return (
     <div>
       <CustomerForm />
