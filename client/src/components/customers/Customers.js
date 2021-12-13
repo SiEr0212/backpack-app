7072e867982baee3e1ad5f8dc741d92c3cc66ac6
SiEr0212 <simonertel@gmx.de>
2021-12-13 23:57:03 +0100

    listing the customers

--- client/src/components/customers/Customers.js
+++ client/src/components/customers/Customers.js
@@ -4,6 +4,10 @@ import CustomerList from "./CustomerList";
 
 export default function Customers() {
   const [customers, setCustomers] = useState([]);
+
+  async function getCustomers() {
+      
+  }
   return (
     <div>
       <CustomerForm />
