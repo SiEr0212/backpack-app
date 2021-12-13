adcbb91243172e2d9b54e877a68217f0a4274af1
SiEr0212 <simonertel@gmx.de>
2021-12-13 23:58:57 +0100

    listing the customers

--- client/src/components/customers/Customers.js
+++ client/src/components/customers/Customers.js
@@ -8,6 +8,7 @@ export default function Customers() {
 
   async function getCustomers() {
     const customerRes = await axios.get("hhtp://localhost:5000/customer/");
+    setCustomers
   }
   return (
     <div>
