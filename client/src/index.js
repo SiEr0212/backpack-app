5a7c537e49c4a58047de81017750035e7d5fd605
SiEr0212 <simonertel@gmx.de>
2021-12-15 23:52:43 +0100

    reformating code

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import axios from "axios";
 
-
-
-
 import App from "./App";
 
 axios.defaults.withCredentials = true;
