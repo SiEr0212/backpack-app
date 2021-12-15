7144e4ecc69897397ea4738a3378c6bd25456b8d
SiEr0212 <simonertel@gmx.de>
2021-12-15 23:52:36 +0100

    reformating code

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
 import Router from "./Router";
 import axios from "axios";
-import {  AuthContextProvider } from "./context/AuthContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 axios.defaults.withCredentials = true;
 
 function App() {
-  return(
-    <AuthContextProvider>  
-    
-    <Router />
-    
+  return (
+    <AuthContextProvider>
+      <Router />
     </AuthContextProvider>
-   
-    
-    
-    ) 
+  );
 }
 
 export default App;
