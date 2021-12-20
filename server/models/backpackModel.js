874a9cffcc272e963f284420eaba5b795aa7b494
SiEr0212 <simonertel@gmx.de>
2021-12-20 23:59:47 +0100

    adding backpack model to db

--- server/models/backpackModel.js
+++ server/models/backpackModel.js
@@ -6,6 +6,7 @@ const backpackSchema = new mongoose.Schema({
   name: { type: String, required: true },
 });
 
+
 const Backpack = mongoose.model("backpack", backpackSchema);
 
 module.exports = Customer;
