a5ec7dcd9646c2a6002c7b644345eac78e3cbb5d
SiEr0212 <simonertel@gmx.de>
2022-02-16 23:57:50 +0100

    adding styling to navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 import LogOutBtn from "../auth/LogOutBtn";
-import Card from "@mui/material/Card";
+import Button from "@mui/material/Card";
 
 export default function Navbar() {
   const { loggedIn } = useContext(AuthContext);
