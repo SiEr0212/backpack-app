4cfbfda839e6687530b149099383204d9da25696
SiEr0212 <simonertel@gmx.de>
2022-02-23 21:20:55 +0100

    working on connecting the list to the db

--- client/src/components/Backpack.js
+++ client/src/components/Backpack.js
@@ -40,7 +40,7 @@ function Backpack() {
   function handleAdd() {
     const newList = list.concat({ weight, name, id: uuidv4() });
     
-    console.log(newList);//data i wantto send to the db
+    console.log(newList);//data I want to send to the db
 
     setList(newList);
 
