111452573af3ddd9681f1138101281eb6b38ca66
SiEr0212 <simonertel@gmx.de>
2022-06-08 00:05:39 +0200

    working on sorting backpack functionality

--- client/src/components/Backpack.js
+++ client/src/components/Backpack.js
@@ -67,7 +67,8 @@ function Backpack() {
 
   function handleSortList() {
     //function to sort items by weight
-    console.log(`handleSortList()${list.weight}`)
+    console.log(`handleSortList${list.map((items)=> items.weight)}`)
+    
     /* const newSortedWeightList = sortedWeightList.sort((a, b) => a - b);
     setSortedWeightList(newSortedWeightList); */
     //todo:  I have to push all the items + weight into an array and sort that array by descending weight (or by user choice by ascending weight)
