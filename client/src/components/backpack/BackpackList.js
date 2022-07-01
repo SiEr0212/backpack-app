760680401c535b3632ff5a51f98ad6edc715ab92
SiEr0212 <simonertel@gmx.de>
2022-07-01 23:16:41 +0200

    creating backpacksoverview

--- client/src/components/backpack/BackpackList.js
+++ client/src/components/backpack/BackpackList.js
@@ -3,7 +3,7 @@ import React from "react";
 function BackpackList({ backpacks }) {
   function renderBackpacks() {
     return backpacks.map((backpack, i) => {
-      return <li key={i}>{backpacks.name}</li>;
+      return <li key={i}>{backpack.name}</li>;
     });
   }
 
