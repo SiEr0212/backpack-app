c85cbd067cdd03e6580cc4843533529e798ecf27
SiEr0212 <simonertel@gmx.de>
2022-07-03 23:51:26 +0200

    working on how to connect backpack to database

--- client/src/components/Backpack.js
+++ client/src/components/Backpack.js
@@ -50,8 +50,7 @@ function Backpack() {
     /* to do:  
         create a global backpack state with redux or context for example and use it in schema? 
         So I can store different backpacks and have acces to it with CRUD functionality
-        need to habe global acces to newList Hook
-  */
+        need to habe global acces to newList Hook */
 
     setName("");
 
