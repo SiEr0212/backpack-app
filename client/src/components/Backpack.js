857ead6b9ae7c166ca4ecb12acc6bf5666dde5d0
SiEr0212 <simonertel@gmx.de>
2022-07-03 23:33:41 +0200

    working on how to connect backpack to database

--- client/src/components/Backpack.js
+++ client/src/components/Backpack.js
@@ -50,7 +50,7 @@ function Backpack() {
     setList(newList);
 
     // initialList.push(newList)//
-    //das durch state update lösen, im moment wird ein komplettes array ins andere gepusht mit doppel items//
+    
 
     setName("");
 
