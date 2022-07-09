a995407a6aceaa307f703212da4cd7f840e23b78
SiEr0212 <simonertel@gmx.de>
2022-07-09 19:34:41 +0200

    add

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   passwordHash: { type: String, required: true },
-  sentenced
+  
 });
 
 const User = mongoose.model("user", userSchema);
