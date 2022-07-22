8f95a1aa22aafeaee88b1d57d57fad3516d690cd
SiEr0212 <simonertel@gmx.de>
2022-07-22 23:53:46 +0200

    working on styling

--- client/src/components/Backpack.js
+++ client/src/components/Backpack.js
@@ -138,7 +138,7 @@ function Backpack() {
       <br />
       <br />
       <div
-        style={{ display: "flex", justifyContent: "center", width: "100vw" }}
+        style={{ display: "flex", justifyContent: "center", width: "50vw" }}
       >
         <Card variant="outlined">
           <IconButton
